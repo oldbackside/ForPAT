ae80a008f776591f84da8714139a4b55d99c3181
oldbackside <thisjiayu@gmail.com>
2019-09-28 14:44:29 +0800

    修改一些错误

--- src/forpat/四数之和.java
+++ src/forpat/四数之和.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class 四数之和 {
-	   public List<List<Integer>> fourSum(int[] nums,int target) {
+	public List<List<Integer>> fourSum(int[] nums,int target) {
 	       Arrays.sort(nums);
 	       List<List<Integer>> ls = new ArrayList<>();
 	       if(nums.length<4) return ls;
@@ -13,32 +13,23 @@ public class 四数之和 {
 	    	  if(a == 0 || (a > 0 && nums[a] != nums[a - 1])) {
 	    	   for(int b=a+1;b<nums.length-2;b++) {
 	    		   if(b== a+1 || (nums[b] != nums[b - 1])) {
-	    		   int c=a+1;
-	    		   if(c==b)c++;
+	    		   int c=b+1;
 	    		   int d=nums.length-1;
 	    	    while(c<d) {
-	    	    	int t=target-nums[a]+nums[b];
-	    	    	if(nums[c]+nums[d]==target) {
+	    	    	int sum=nums[a]+nums[b]+nums[c]+nums[d];
+	    	    	if(sum==target) {
 	    	    		 ls.add(Arrays.asList(nums[a], nums[b], nums[c],nums[d]));
-	    	    		  while(nums[c]==nums[c+1]&&c!=a&&c<d) {c++;
-	    	    		  if(c==nums.length-1)break;
-	    	    		  }
-	    	      		while(nums[d]==nums[d-1]&&d!=a&&c<d) {d--;
-	    	      		if(d==0) break;
-	    	      		}
+	    	    		  while(c<d&&nums[c]==nums[c+1]) c++;
+	    	      		while(c<d&&nums[d]==nums[d-1]) d--;
 	    	    		 c++;
 	    	    		 d--;
 	    	    	}
-	    	    	else if(t<0) {
-	    	    	     while(nums[c]==nums[c+1]&&c!=a&&c<d) {c++;
-	    	    	     if(c==nums.length-1)break;
-	    	    	     }
+	    	    	else if(sum<target) {
+	    	    	    //  while(c<d&&nums[c]==nums[c+1]) c++;
 	    	    	     c++;
 	    	    	}
 	    	    	else {
-	    	    		while(nums[d]==nums[d-1]&&d!=a&&c<d) {d--;
-	    	    		if(d==0)break;
-	    	    		}
+	    	    	//	while(c<d&&nums[d]==nums[d-1]) d--;
 	    	    		d--;
 	    	    	}
 	    	    }
